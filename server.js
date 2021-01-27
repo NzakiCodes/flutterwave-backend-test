c382b12151eec84341ac6d9c66e39d46b1f924f2
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2021-01-27 15:57:06 +0100

    Added Express

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server working 🔥");
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => `Server running on ${port} port 🔥`);
